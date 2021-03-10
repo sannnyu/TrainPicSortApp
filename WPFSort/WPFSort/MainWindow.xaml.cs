da620c381c39fc769afa9a6ef7f5d9b1c821523b
SangYe <sangyu.li@outlook.com>
2021-03-10 22:49:28 +0800

    Update UI
    Adaptive UI

---
+++ WPFSort/WPFSort/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFSort
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void numberList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+
+        }
+
+        //显示拍好的14张车号照片
+        public void Display()
+        {
+            //Image image = new Image(new Uri(""));
+            
+        }
+        
+    }
+}
